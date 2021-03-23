19160dd9c6437b480a7275e18655968e09b95fbd
Vins <sharmavins23@gmail.com>
2021-03-22 23:53:28 -0400

    added portion comments

--- bits.h
+++ bits.h
@@ -1,28 +1,28 @@
-// 1
+// 1st Portion - 4 functions
 int bitXor(int, int);
 int test_bitXor(int, int);
 int tmin();
 int test_tmin();
-// 2
+// 2nd Portion - 4 functions
 int isTmax(int);
 int test_isTmax(int);
 int allOddBits();
 int test_allOddBits();
 int negate(int);
 int test_negate(int);
-// 3
+// 3rd Portion - 6 functions
 int isAsciiDigit(int);
 int test_isAsciiDigit(int);
 int conditional(int, int, int);
 int test_conditional(int, int, int);
 int isLessOrEqual(int, int);
 int test_isLessOrEqual(int, int);
-// 4
+// 4th Portion - 4 functions
 int logicalNeg(int);
 int test_logicalNeg(int);
 int howManyBits(int);
 int test_howManyBits(int);
-// float
+// Float portion - 6 functions
 unsigned floatScale2(unsigned);
 unsigned test_floatScale2(unsigned);
 int floatFloat2Int(unsigned);
